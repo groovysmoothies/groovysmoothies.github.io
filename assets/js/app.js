685f317260940769abccf4444765fda179e062d5
Janice Pang <janice.jamye.pang@gmail.com>
2015-05-26 04:35:44 -0700

    DRAG AND DROP GOODNESS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,48 @@
+var limit = 5;
+var counter = limit;
+var colors = ['blue', 'yellow', 'red'];
+
+var groovy = false;
+
+function getRandomColor() {
+  return colors[Math.floor(colors.length * Math.random())];
+}
+
+function getNewCount(counter) {
+  return 1 + Math.floor(Math.random() * counter);
+}
+
+function switchRandomColor(currentColor) {
+  var newcolor = getRandomColor();
+
+  while (newcolor == currentColor) newcolor = getRandomColor();
+
+  return newcolor;
+}
+
+
+
+function updateText() {
+  document.getElementById('fruitNumber').innerHTML = currentCount.toString();
+  document.getElementById('fruitColor').innerHTML = currentColor;
+  document.getElementById('count').innerHTML = limit - counter;
+}
+
+
+
+var currentColor = getRandomColor();
+var currentCount = getNewCount(counter);
+updateText();
+
+
+function blend() {
+  if (groovy) {
+    document.getElementById('game').dispatchEvent(new Event('shunt'));
+  } else {
+
+  }
+}
+
 function fruitdrag(e, color) {
   e.dataTransfer.setData("color", color);
 }
@@ -5,7 +50,25 @@ function fruitdrag(e, color) {
 function fruitdrop(e) {
   e.preventDefault();
 
-  console.log(e.dataTransfer.getData("color"));
+  c = e.dataTransfer.getData("color");
+
+  if ((c == currentColor) && (!groovy)) {
+    --currentCount;
+    --counter;
+
+    if (counter == 0) {
+      groovy = true;
+    }
+
+    if (currentCount == 0) {
+      currentColor = switchRandomColor(currentColor);
+      currentCount = getNewCount(counter);
+    }
+
+    updateText();
+  } else {
+
+  }
 }
 
 function fruitallowdrop(e) {
