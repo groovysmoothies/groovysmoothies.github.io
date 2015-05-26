1d7e52e819559b452d827d32d6232619ef885cea
Janice Pang <janice.jamye.pang@gmail.com>
2015-05-26 00:35:51 -0700

    adding stuff to githubgit statusgit statusgit status

---
+++ assets/js/shunts.js
@@ -0,0 +1,20 @@
+groovyShunt = new ShuntDiv(document.getElementById('main-container'), [
+    new ShuntDiv.Transition('intro', 'game' , 'enterAnimateCss', 'click', {
+        id: 'next-button',
+        animation_name: 'fadeInUp',
+        animation_time: 1000,
+        animation_function: 'cubic-bezier(.1,1,.61,.96)',
+    }),
+
+    new ShuntDiv.Transition('game', 'result' , 'dualAnimateCss', 'event', {
+        eventName: 'shunt',
+        exit_animation_name: 'fadeOut',
+        exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_animation_name: 'slideInDown',
+        enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_above: true,
+    }),
+], {
+    default: 'intro',
+    saveWithHash: true,
+});
