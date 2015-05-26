ceb9daf79e5819a5b8d22695f3ea76814e46bf12
Janice Pang <janice.jamye.pang@gmail.com>
2015-05-26 15:49:12 -0700

    update google font weight

--- assets/js/shunts.js
+++ assets/js/shunts.js
@@ -6,11 +6,22 @@ groovyShunt = new ShuntDiv(document.getElementById('main-container'), [
         animation_function: 'cubic-bezier(.1,1,.61,.96)',
     }),
 
-    new ShuntDiv.Transition('game', 'result' , 'dualAnimateCss', 'event', {
+    new ShuntDiv.Transition('game', 'transition' , 'dualAnimateCss', 'event', {
         eventName: 'shunt',
         exit_animation_name: 'fadeOut',
         exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
         enter_animation_name: 'slideInDown',
+        enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_above: true,
+    }),
+
+    new ShuntDiv.Transition('transition', 'result' , 'dualAnimateCss', 'event', {
+        id: 'blendAnother',
+        exit_animation_name: 'fadeOut',
+        exit_animation_time: 3000,
+        exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_animation_name: 'fadeIn',
+        enter_animation_time: 3000,
         enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
         enter_above: true,
     }),
