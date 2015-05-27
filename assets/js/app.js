8d56957d5148f3582c246182a4dbd61e7d4b3426
Janice Pang <janice.jamye.pang@gmail.com>
2015-05-26 17:27:45 -0700

    fix rawgit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,19 @@
 // ♫ LET'S GROOVE ♫
 // ♫ feat DJ KLAVN ♫
 
-var limit = 6;
-var counter = limit;
-var colors = ['blue', 'yellow', 'red', 'orange', 'green', 'purple'];
 
-var colorBlocks = document.querySelectorAll('.colorBlock');
+// initialize global variables
+
+var limit = 6;
+var counter = limit; // counting down for colorblocks
+var colors = ['blue', 'yellow', 'red', 'orange', 'green', 'purple']; // colors of color blocks
+
+var colorBlocks = document.querySelectorAll('.colorBlock'); // setting color block color
 
 var groovy = false;
+
+
+// randomize Gameplay variables
 
 function getRandomColor() {
   return colors[Math.floor(colors.length * Math.random())];
@@ -26,11 +32,17 @@ function switchRandomColor(currentColor) {
   return newcolor;
 }
 
+
+// initializing Gameplay variables
+
 var instructions = document.getElementById('instructions');
 var fruitNumber = document.getElementById('fruitNumber');
 var fruitColor = document.getElementById('fruitColor');
 var fruitWord = document.getElementById('fruitWord');
 var count = document.getElementById('count');
+
+
+// update Gameplay text
 
 function updateText() {
   var instructionsAreHidden = instructions.classList.contains("hidden");
@@ -42,18 +54,33 @@ function updateText() {
 }
 
 
+// initializing color + number of fruit
 
 var currentColor = getRandomColor();
 var currentCount = getNewCount(counter);
 updateText();
 
 
+
+//--------------------------------------- dispatch shunt event --> gameplay --> transition screen
+
+var transitionFrame = document.getElementById('transition');
+
+
 function blend() {
   if (groovy) {
     document.getElementById('game').dispatchEvent(new Event('shunt'));
+
+      // triggers gameplay --> transition --> result
+      setTimeout(function(){
+      console.log("transitioning");
+          transitionFrame.dispatchEvent(new Event('shunt'));
+      }, 5000);
   } else {
   }
 }
+
+// drag + drop fruits
 
 function fruitdrag(e, colors) {
   e.dataTransfer.setData("colors", JSON.stringify(colors));
@@ -72,16 +99,17 @@ function fruitdrop(e) {
     cb.classList.remove('clear');
     cb.classList.add(currentColor);
 
-
+    // check if all 6 fruits have been placed in cup
 
     if (counter == 0) {
         document.getElementById('blendButton').classList.remove('inactive');
         document.getElementById('congrats').classList.remove('hidden');
         document.getElementById('instructions').classList.add('hidden');
-      groovy = true;
+        groovy = true;
     }
 
 
+    // update new colors + number of fruits
 
     if (currentCount == 0) {
       currentColor = switchRandomColor(currentColor);
@@ -95,9 +123,15 @@ function fruitdrop(e) {
   }
 }
 
+
+// we got this
+
 function fruitallowdrop(e) {
   e.preventDefault();
 }
+
+
+// resetting the GAME
 
 var blendButton = document.getElementById('blendButton');
 var congrats = document.getElementById('congrats');
--- assets/js/shunts.js
+++ assets/js/shunts.js
@@ -2,37 +2,37 @@ groovyShunt = new ShuntDiv(document.getElementById('main-container'), [
     new ShuntDiv.Transition('intro', 'game' , 'enterAnimateCss', 'click', {
         id: 'next-button',
         animation_name: 'fadeInUp',
-        animation_time: 1000,
+        animation_time: 600,
         animation_function: 'cubic-bezier(.1,1,.61,.96)',
     }),
 
-    new ShuntDiv.Transition('game', 'result' , 'dualAnimateCss', 'event', {
+    new ShuntDiv.Transition('game', 'transition' , 'dualAnimateCss', 'event', {
         eventName: 'shunt',
         exit_animation_name: 'fadeOut',
         exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
-        enter_animation_name: 'slideInDown',
+        enter_animation_name: 'fadeIn',
         enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
         enter_above: true,
     }),
 
-//    new ShuntDiv.Transition('transition', 'result' , 'dualAnimateCss', 'event', {
-//        id: 'blendAnother',
-//        exit_animation_name: 'fadeOut',
-//        exit_animation_time: 3000,
-//        exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
-//        enter_animation_name: 'fadeIn',
-//        enter_animation_time: 3000,
-//        enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
-//        enter_above: true,
-//    }),
+    new ShuntDiv.Transition('transition', 'result' , 'dualAnimateCss', 'event', {
+        eventName: 'shunt',
+        exit_animation_name: 'fadeOut',
+        exit_animation_time: 1500,
+        exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_animation_name: 'fadeIn',
+        enter_animation_time: 1500,
+        enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
+        enter_above: true,
+    }),
 
     new ShuntDiv.Transition('result', 'intro' , 'dualAnimateCss', 'click', {
         id: 'blendAnother',
         exit_animation_name: 'fadeOut',
-        exit_animation_time: 3000,
+        exit_animation_time: 1500,
         exit_animation_function: 'cubic-bezier(.1,1,.61,.96)',
         enter_animation_name: 'fadeIn',
-        enter_animation_time: 3000,
+        enter_animation_time: 1500,
         enter_animation_function: 'cubic-bezier(.1,1,.61,.96)',
         enter_above: true,
     }),
